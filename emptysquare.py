70b87679893cc43e4a3a3814b94c20b454683110
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2010-09-21 23:41:06 -0400

    Rely on jQuery BBQ to watch changes in URL and support back button, further clean up emptysquare.js

--- emptysquare.py
+++ emptysquare.py
@@ -97,7 +97,7 @@ class PhotoForFacebookHandler(tornado.web.RequestHandler):
                 current_slug=slug,
                 current_set_index=current_set_index,
                 photos=emptysquare_set_photos(slug),
-		# Remember that the photo index comes in 1-indexed
+                # Remember that the photo index comes in 1-indexed
                 photo_index=int(photo_index)-1
             )
         else:
